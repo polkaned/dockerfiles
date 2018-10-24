33fe9ccc07ef6020bb3200724f09c40f711721d0
polkaned <benjamin@polkaned.net>
2018-10-24 18:55:07 +0200

    first commit

---
+++ expressvpn/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+cp /etc/resolv.conf /tmp/resolv.conf
+su -c 'umount /etc/resolv.conf'
+cp /tmp/resolv.conf /etc/resolv.conf
+service expressvpn restart
+/usr/bin/expect /tmp/expressvpnActivate.sh
+expressvpn connect
+exec "$@"
