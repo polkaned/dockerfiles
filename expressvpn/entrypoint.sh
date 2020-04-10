691106d723d904482988b27175bcc846a12b5812
Pierre Yves <lozachmp@gmail.com>
2020-04-10 20:03:50 +0200

    change

--- expressvpn/entrypoint.sh
+++ expressvpn/entrypoint.sh
@@ -5,8 +5,7 @@ su -c 'umount /etc/resolv.conf'
 cp /tmp/resolv.conf /etc/resolv.conf
 service expressvpn restart
 /usr/bin/expect /tmp/expressvpnActivate.sh
-expressvpn protocol udp
-expressvpn preferences set network_lock on
-expressvpn preferences set force_vpn_dns false
+expressvpn protocol tdp
+expressvpn preferences set force_vpn_dns true
 expressvpn connect $SERVER
 exec "$@"
