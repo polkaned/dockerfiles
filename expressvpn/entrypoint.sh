216650a87e21abbcbdd836491d5c493c5bc37533
Pierre Yves <lozachmp@gmail.com>
2020-04-11 22:35:06 +0200

    testing default conf

--- expressvpn/entrypoint.sh
+++ expressvpn/entrypoint.sh
@@ -5,7 +5,5 @@ su -c 'umount /etc/resolv.conf'
 cp /tmp/resolv.conf /etc/resolv.conf
 service expressvpn restart
 /usr/bin/expect /tmp/expressvpnActivate.sh
-expressvpn protocol udp
-expressvpn preferences set force_vpn_dns true
 expressvpn connect $SERVER
 exec "$@"
