d7c835748fb4e16cd3ccfaf473dcf0f28ce18a94
polkaned <benjamin@polkaned.net>
2021-02-19 22:20:07 +0100

    Update ExpressVPN client version

--- expressvpn/entrypoint.sh
+++ expressvpn/entrypoint.sh
@@ -3,6 +3,7 @@
 cp /etc/resolv.conf /tmp/resolv.conf
 su -c 'umount /etc/resolv.conf'
 cp /tmp/resolv.conf /etc/resolv.conf
+sed -i 's/DAEMON_ARGS=.*/DAEMON_ARGS=""/' /etc/init.d/expressvpn
 service expressvpn restart
 /usr/bin/expect /tmp/expressvpnActivate.sh
 expressvpn connect $SERVER
