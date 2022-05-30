ed6a55c80c321d3cc1cad2b0131ec6c336a0a018
Toby <tobyxdd@gmail.com>
2022-05-30 15:50:51 -0700

    handle the case where expressvpn is already activated

--- expressvpn/expressvpnActivate.sh
+++ expressvpn/expressvpnActivate.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/expect
 spawn expressvpn activate
-expect "code:"
-send "$env(ACTIVATION_CODE)\r"
-expect "information."
-send "n\r"
+expect {
+    "code:" {
+        send "$env(ACTIVATION_CODE)\r"
+        expect "information."
+        send "n\r"
+    }
+    "Already activated" {send "n\r"}
+}
 expect eof
